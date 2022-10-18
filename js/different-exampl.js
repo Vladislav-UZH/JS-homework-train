84a0879cf7953c2b89646b84812a62048be6040a
Vladislav-UZH <perebziak.vladislav@gmail.com>
2022-10-18 18:16:35 +0300

    о

--- js/different-exampl.js
+++ js/different-exampl.js
@@ -134,10 +134,260 @@
 // console.dir(calculator);
 // --------------------------------------------------------------------------------------------
 
+/*Дан массив с числами. 50 чисел Наполняем, как в прошлом задании. С помощью цикла проверьте, что в нем есть элемент со значением которое пользователь вводит в prompt("Введите число от 0 до 50").
+ Как только будет найден первый такой элемент - выведите 'Есть'.
+  Если такого элемента нет - вывести alert('Такого числа нет в массиве').*/
+// Дополнительно сделать проверку на число, если введено число
+// const arr = [];
+// for (let i = 0; i <= 50; i+=1) {
+//     arr.push(i)
+    
+// }
+
+// const inputNum = Number(prompt('Enter your number!'));
+
+
+// if (typeof inputNum === 'number') {
+//         if ( arr.includes(inputNum)) {
+//             alert(`Yes, your entered number (${inputNum}) is in collection!`);
+            
+//         } else {
+//             alert(`Sorry, I can not find the number`)
+//         };
+// } else {
+//     alert(`${inputNum} is not correct! Enter only number!`)
+// };
+
+
+// console.table(arr)
 
 
 
 
+// --------------------------------------------------------------------------------------------
+/* Є масив із числами. Створіть із нього новий масив,
+ де залишаться лежати лише позитивні числа. Створіть для цього допоміжну функцію isPositive(),
+  яка параметром прийматиме число і повертатиме true, якщо число позитивне, і false - якщо негативне.
+//  */
+//
+// const arr = [-1,-3,-4,0,1,2,5,9];
+// const isPositive = num => num > 0 ? true : false;
+
+// for (let i = 0; i < arr.length; i += 1) {
+    
+//     // if (isPositive(arr[i])) {
+//     //      positiveNums.push(arr[i])
+//     // }
+//     isPositive(arr[i]) ? positiveNums.push(arr[i]) : console.log('Error');
+
+    
+// }
+// console.log(positiveNums)
+
+
+
+// const isPositive = arr.filter(num => num > 0);
+// console.log(isPositive)
+// --------------------------------------------------------------------------------------------
+//  Реализуйте класс Student(Студент), который будет наследовать от класса User.
+// Этот класс должен иметь следующие свойства: name(имя, наследуется от User),
+// surname(фамилия, наследуется от User), year(год поступления в вуз).
+// Класс должен иметь метод getFullName()(наследуется от User),
+// с помощью которого можно вывести одновременно имя и фамилию студента.
+// Также класс должен иметь метод getCourse(), который будет выводить текущий курс
+// студента(от 1 до 5).Курс вычисляется так: нужно от текущего года отнять год поступления в вуз.
+// Текущий год получите самостоятельно.
+
+// class User{
+//     constructor({name, surname, year}){
+//         this.name = name;
+//         this.surname = surname;
+//         this.year = year;
+//     }
+//     getFullName() { return `${this.name} ${this.surname}` }
+//     getCourse() { return 2022 - this.year}
+// }
+
+// class Student extends User {
+//     constructor(name, surname, year) {
+//         super(name, surname, year)
+//     }
+// }
+
+// const student = new Student({name:'Den', surname:'Иванов', year:2019});
+
+// console.log(student.getFullName()); //выведет 'Иван'
+// console.log(worker.surname); //выведет 'Иванов'
+// console.log(worker.getFullName()); //выведет 'Иван Иванов'
+// console.log(worker.year); //выведет 2019console.log(worker.getCourse()); //выведет 3 - третий курс, так как текущий год 2022
+
+// --------------------------------------------------------------------------------------------
+// const users = [
+// 	{
+// 		name: 'Moore Hensley',
+// 		email: 'moorehensley@indexia.com',
+// 		eyeColor: 'blue',
+// 		friends: ['Sharron Pace'],
+// 		isActive: false,
+// 		balance: 2811,
+// 		gender: 'male',
+// 	},
+// 	{
+// 		name: 'Sharlene Bush',
+// 		email: 'sharlenebush@tubesys.com',
+// 		eyeColor: 'blue',
+// 		friends: ['Briana Decker', 'Sharron Pace'],
+// 		isActive: true,
+// 		balance: 3821,
+// 		gender: 'female',
+// 	},
+//     {
+// 		name: 'Ross Vazquez',
+// 		email: 'rossvazquez@xinware.com',
+// 		eyeColor: 'green',
+// 		friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+// 		isActive: false,
+// 		balance: 3793,
+// 		gender: 'male',
+// 	},
+// 	{
+// 		name: 'Elma Head',
+// 		email: 'elmahead@omatom.com',
+// 		eyeColor: 'green',
+// 		friends: ['Goldie Gentry', 'Aisha Tran'],
+// 		isActive: true,
+// 		balance: 2278,
+// 		gender: 'female',
+// 	},
+// 	{
+// 		name: 'Carey Barr',
+// 		email: 'careybarr@nurali.com',
+// 		eyeColor: 'blue',
+// 		friends: ['Jordan Sampson', 'Eddie Strong', 'Adrian Cross'],
+// 		isActive: true,
+// 		balance: 3951,
+// 		gender: 'male',
+// 	},
+//     {
+// 		name: 'Blackburn Dotson',
+// 		email: 'blackburndotson@furnigeer.com',
+// 		eyeColor: 'brown',
+// 		friends: [
+// 			'Jacklyn Lucas',
+// 			'Linda Chapman',
+// 			'Adrian Cross',
+// 			'Solomon Fokes',
+// 		],
+// 		isActive: false,
+// 		balance: 1498,
+// 		gender: 'male',
+// 	},
+// 	{
+// 		name: 'Sheree Anthony',
+// 		email: 'shereeanthony@kog.com',
+// 		eyeColor: 'brown',
+// 		friends: ['Goldie Gentry', 'Briana Decker'],
+// 		isActive: true,
+// 		balance: 2764,
+// 		gender: 'female',
+// 	},
+// ];
+
+// const base = {};
+
+// const keys = Object.keys(users[0])
+// for (const key of keys) {
+//     base[key] = []
+// }
+// // console.log(base)
+
+// const restAllValues = (users) => {
+//     for (const element in base) {
+//         console.log(element)
+//         for (const user of users) {
+//             base[element].push(user[element])
+//         }
+//     }
+//     return base;
+// };
+// console.log("🚀 ~ file: different-exampl.js ~ line 315 ~ restAllValues(users)", restAllValues(users))
+
+
+// balance: [2811, 3821, 3793, 2278, 3951, 1498, 2764]
+// email: ['moorehensley@indexia.com', 'sharlenebush@tubesys.com', 'rossvazquez@xinware.com', 'elmahead@omatom.com', 'careybarr@nurali.com', 'blackburndotson@furnigeer.com', 'shereeanthony@kog.com']
+// eyeColor: ['blue', 'blue', 'green', 'green', 'blue', 'brown', 'brown']
+// friends: [Array(1), Array(2), Array(3), Array(2), Array(3), Array(4), Array(2)]
+// gender: ['male', 'female', 'male', 'female', 'male', 'male', 'female']
+// isActive: [false, true, false, true, true, false, true]
+// name: ['Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony']
+
+// --------------------------------------------------------------------------------------------
+//Напишите две функции
+//1)
+// letMeSeeYourName(callback) - спрашивает имя пользователя
+//через prompt и вызывает callback функцию.
+// 2)
+//greet(name) - коллбек принимающий имя и логирующий в консоль
+//строку "Привет <name>"
+//Реализуй проверку, что prompt не пустой
+
+
+// const letMeSeeYourName = (callback) => {
+//   const inputUser = prompt('Enter your name, please!');
+//   if (inputUser) {
+//     return callback(inputUser);
+//   }
+//   alert('Canceled by user');
+//   return console.log('Canceled by user');
+// }
+
+// const greet = (name) => console.log('Hello,', name);
+
+// letMeSeeYourName(greet);
+
+// --------------------------------------------------------------------------------------------
+//    ЗАМЫКАНИЯ    ЗАМИКАННЯ    CLOSURES    
+
+
+//Выполни рефакторинг makeDish так, чтобы не нужно было
+//каждый раз передавать имя шефа.
+//Напишите функцию makeShef(shefName) которая возвращает функцию
+//makeDish(dish), помнящую имя шефа при её вызове
+
+// const makeDish = function (shefName, dish) {
+//   console.log(`${shefName} is cooking ${dish}`);
+// };
+
+// const makeShef = shefName => {
+//   return function makeDish(dish) {
+//      console.log(`${shefName} is cooking ${dish}`);
+//   }
+  
+// }
+// const poly = makeShef('Poly')
+// poly('Cake')
+// const jack = makeShef('Jack')
+// jack('pizza')
+
+// --------------------------------------------------------------------------------------------
+
+
+
+// --------------------------------------------------------------------------------------------
+
+
+
+
+// --------------------------------------------------------------------------------------------
+
+
+
+// --------------------------------------------------------------------------------------------
+
+
+
+
+// --------------------------------------------------------------------------------------------
 
 
 
--- js/thisAndProto.js
+++ js/thisAndProto.js
@@ -72,14 +72,119 @@
 // storage.removeItem("Prolonger");
 // console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
 // ---------------------------------------------------------------------------------------------------
+/* Реализуйте класс Worker (Работник), который будет иметь следующие свойства: name (имя), surname (фамилия), rate (ставка за день работы), days (количество отработанных дней).
+ Также класс должен иметь метод getSalary(),который будет выводить зарплату работника.
+  Зарплата - это произведение (умножение) ставки rate на количество отработанных дней days.
+С помощью нашего класса создайте двух рабочих и найдите сумму их зарплат.
+ Вот так должен работать наш класс:
+*/
 
+// class Worker {
+//     constructor({name, surname, rate, days} = {}) {
+//         this.name = name;
+//         this.surname = surname;
+//         this.rate = rate;
+//         this.days = days;
+//     }
+//     getSalary() {
+//         return this.rate * this.days;
+//     }
+// }
+
+
+
+// const worker = new Worker({name: 'Иван', surname: 'Иванов', rate: 10, days: 31});
+// console.log(worker.getSalary())
+// const worker1 = new Worker({name: 'Den', surname: 'Иванов', rate: 20, days: 314});
+// console.log( worker.getSalary() + worker1.getSalary())
+
+// ---------------------------------------------------------------------------------------------------
+
+// 2. Модифицируйте класс Worker из предыдущей задачи следующим образом:
+//   сделайте все его свойства приватными, а для их чтения сделайте методы - геттеры.
+//   Наш класс теперь будет работать так:
+
+// const worker = new Worker('Иван', 'Иванов', 10, 31);
+
+// console.log(worker.getName()); //выведет 'Иван'
+// console.log(worker.getSurname()); //выведет 'Иванов'
+// console.log(worker.getRate()); //выведет 10
+// console.log(worker.getDays()); //выведет 31
+// console.log(worker.getSalary()); //выведет 310 - то есть 10*31
+// 3. Модифицируйте класс Worker из предыдущей задачи следующим образом:
+// для свойства rate и для свойства days сделайте еще и методы - сеттеры.
+
+// Наш класс теперь будет работать так:
+
+// worker = new Worker('Иван', 'Иванов', 10, 31);
+
+// console.log(worker.getRate()); //выведет 10
+// console.log(worker.getDays()); //выведет 31
+// console.log(worker.getSalary()); //выведет 310 - то есть 10*31
+
+// //Теперь используем сеттер:
+// worker.setRate(20); //увеличим ставку
+// worker.setDays(10); //уменьшим дни
+// console.log(worker.getSalary()); //выведет 200 - то есть 20*10.
+
+// class Worker {
+//     #name;
+//     #surname;
+//     #rate;
+//     #days;
+//     constructor({name, surname, rate, days} = {}) {
+//         this.#name = name;
+//         this.#surname = surname;
+//         this.#rate = rate;
+//         this.#days = days;
+//     }
+
+//     get thisName() {
+//     return this.#name;
+ 
+//     };
+//     set thisName(newName) {
+//         this.#name = newName;
+//     }
+//        get thisSurname() {
+//            return this.#surname;
+//     };
+//     set thisSurname(newSurname) {
+//         this.#surname = newSurname;
+//     }
+//        get thisRate() {
+//            return this.#rate;
+//     };
+//     set thisRate(newRate) {
+//         this.#rate = newRate;
+//     }
+//        get thisDays() {
+//            return this.#days;
+//     };
+//     set thisDays(newDays) {
+//         this.#days = newDays
+//     }
+//     getSalary() {
+//         return this.#rate * this.#days;
+//     }
     
+// }
+
+
+
+// const worker = new Worker({name: 'Иван', surname: 'Иванов', rate: 10, days: 31});
+// console.log(worker.getSalary())
+// const worker1 = new Worker({name: 'Den', surname: 'Иванов', rate: 20, days: 314});
+// console.log( worker.getSalary() + worker1.getSalary())
+// console.log(worker.thisDays = 2)
+// console.log(worker.thisRate = 3)
+// console.log(worker.getSalary())
 
 // ---------------------------------------------------------------------------------------------------
 
 
 
-// ---------------------------------------------------------------------------------------------------
+
 
 
 
